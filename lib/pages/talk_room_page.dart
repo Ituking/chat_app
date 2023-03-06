07b190c31f3250d03cf50cede5b88d3d9ba89aff
Ituking <77528963+Ituking@users.noreply.github.com>
2023-03-07 06:55:00 +0900

    メッセージ送信フィールドの作成（トークルームページ）
    一番最新のメッセージがメッセージ送信フィールドに隠れてしまっている問題を修正

--- lib/pages/talk_room_page.dart
+++ lib/pages/talk_room_page.dart
@@ -129,51 +129,54 @@ class _TalkRoomPageState extends State<TalkRoomPage> {
         child: Stack(
           alignment: Alignment.bottomCenter,
           children: [
-            ListView.builder(
-                physics: const RangeMaintainingScrollPhysics(),
-                shrinkWrap: true,
-                reverse: true,
-                itemCount: messageList.length,
-                itemBuilder: (context, index) {
-                  return Padding(
-                    padding: EdgeInsets.only(
-                        top: 10,
-                        left: 10,
-                        right: 10,
-                        bottom: index == 0 ? 10 : 0),
-                    child: Row(
-                      crossAxisAlignment: CrossAxisAlignment.end,
-                      textDirection: messageList[index].isMe
-                          ? TextDirection.rtl
-                          : TextDirection.ltr,
-                      children: [
-                        Container(
-                          constraints: BoxConstraints(
-                              maxWidth:
-                                  MediaQuery.of(context).size.width * 0.6),
-                          decoration: BoxDecoration(
-                            color: messageList[index].isMe
-                                ? const Color.fromARGB(194, 158, 158, 158)
-                                : const Color.fromARGB(255, 12, 151, 107),
-                            borderRadius: BorderRadius.circular(15),
-                          ),
-                          padding: const EdgeInsets.symmetric(
-                              horizontal: 10, vertical: 6),
-                          child: Text(
-                            messageList[index].message,
-                            style: TextStyle(
+            Padding(
+              padding: const EdgeInsets.only(bottom: 100),
+              child: ListView.builder(
+                  physics: const RangeMaintainingScrollPhysics(),
+                  shrinkWrap: true,
+                  reverse: true,
+                  itemCount: messageList.length,
+                  itemBuilder: (context, index) {
+                    return Padding(
+                      padding: EdgeInsets.only(
+                          top: 10,
+                          left: 10,
+                          right: 10,
+                          bottom: index == 0 ? 10 : 0),
+                      child: Row(
+                        crossAxisAlignment: CrossAxisAlignment.end,
+                        textDirection: messageList[index].isMe
+                            ? TextDirection.rtl
+                            : TextDirection.ltr,
+                        children: [
+                          Container(
+                            constraints: BoxConstraints(
+                                maxWidth:
+                                    MediaQuery.of(context).size.width * 0.6),
+                            decoration: BoxDecoration(
                               color: messageList[index].isMe
-                                  ? Colors.black
-                                  : const Color.fromARGB(230, 255, 255, 255),
+                                  ? const Color.fromARGB(194, 158, 158, 158)
+                                  : const Color.fromARGB(255, 12, 151, 107),
+                              borderRadius: BorderRadius.circular(15),
+                            ),
+                            padding: const EdgeInsets.symmetric(
+                                horizontal: 10, vertical: 6),
+                            child: Text(
+                              messageList[index].message,
+                              style: TextStyle(
+                                color: messageList[index].isMe
+                                    ? Colors.black
+                                    : const Color.fromARGB(230, 255, 255, 255),
+                              ),
                             ),
                           ),
-                        ),
-                        Text(intl.DateFormat('HH:mm')
-                            .format(messageList[index].sendTime)),
-                      ],
-                    ),
-                  );
-                }),
+                          Text(intl.DateFormat('HH:mm')
+                              .format(messageList[index].sendTime)),
+                        ],
+                      ),
+                    );
+                  }),
+            ),
             Column(
               mainAxisSize: MainAxisSize.min,
               children: [
