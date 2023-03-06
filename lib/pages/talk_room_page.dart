92c17f6cc643f2efffc2d5546b1cf59dd11bebcc
Ituking <77528963+Ituking@users.noreply.github.com>
2023-03-07 00:17:31 +0900

    リスト表示の実装（トークルームページ）

--- lib/pages/talk_room_page.dart
+++ lib/pages/talk_room_page.dart
@@ -1,4 +1,6 @@
+import 'package:chat_app/model/message.dart';
 import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
 
 class TalkRoomPage extends StatefulWidget {
   final String name;
@@ -9,12 +11,34 @@ class TalkRoomPage extends StatefulWidget {
 }
 
 class _TalkRoomPageState extends State<TalkRoomPage> {
+  List<Message> messageList = [
+    Message(
+      message: "Hello.",
+      isMe: true,
+      sendTime: DateTime(2023, 3, 6, 10, 58),
+    ),
+    Message(
+      message: "Hello.",
+      isMe: false,
+      sendTime: DateTime(2023, 3, 6, 12, 24),
+    ),
+  ];
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text(widget.name),
       ),
+      body: ListView.builder(
+          itemCount: messageList.length,
+          itemBuilder: (context, index) {
+            return Row(
+              children: [
+                Text(messageList[index].message),
+                Text(DateFormat('HH:mm').format(messageList[index].sendTime)),
+              ],
+            );
+          }),
     );
   }
 }
