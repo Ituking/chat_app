52d46e6f3d8f8bd288ba1728619739efaa81dc04
Ituking <77528963+Ituking@users.noreply.github.com>
2023-03-07 02:21:11 +0900

    チャットバブルの色調整（トークルームページ）

--- lib/pages/talk_room_page.dart
+++ lib/pages/talk_room_page.dart
@@ -154,7 +154,7 @@ class _TalkRoomPageState extends State<TalkRoomPage> {
                           decoration: BoxDecoration(
                             color: messageList[index].isMe
                                 ? const Color.fromARGB(194, 158, 158, 158)
-                                : const Color.fromARGB(255, 14, 229, 161),
+                                : const Color.fromARGB(255, 15, 196, 139),
                             borderRadius: BorderRadius.circular(15),
                           ),
                           padding: const EdgeInsets.symmetric(
