1a249e942bf7565442ecb3ae94fa9b995a605f09
Ituking <77528963+Ituking@users.noreply.github.com>
2023-03-06 18:49:24 +0900

    talk_room_page.dartの作成

---
+++ lib/pages/talk_room_page.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/src/widgets/framework.dart';
+import 'package:flutter/src/widgets/placeholder.dart';
+
+class TalkRoomPage extends StatefulWidget {
+  const TalkRoomPage({super.key});
+
+  @override
+  State<TalkRoomPage> createState() => _TalkRoomPageState();
+}
+
+class _TalkRoomPageState extends State<TalkRoomPage> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
