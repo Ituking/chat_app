5465fe1124dd832603a1f0c728e5d135b0f428a8
Ituking <77528963+Ituking@users.noreply.github.com>
2023-03-08 23:40:32 +0900

    setting_profile_page.dartの作成

---
+++ lib/pages/setting_profile_page.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class SettingProfilePage extends StatefulWidget {
+  const SettingProfilePage({super.key});
+
+  @override
+  State<SettingProfilePage> createState() => _SettingProfilePageState();
+}
+
+class _SettingProfilePageState extends State<SettingProfilePage> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
