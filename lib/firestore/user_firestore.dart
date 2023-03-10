7e40c0edc7c6dc572ff57ee31e9cb959a8a0f8e6
Ituking <77528963+Ituking@users.noreply.github.com>
2023-03-11 03:46:31 +0900

    アカウント作成時に他ユーザーとのトークルームを作成
    createUser()の時点でnewDocにユーザーを追加する処理が格納されるように定義

--- lib/firestore/user_firestore.dart
+++ lib/firestore/user_firestore.dart
@@ -6,21 +6,24 @@ class UserFirestore {
       FirebaseFirestore.instance;
   static final _userCollection = _firebaseFirestoreInstance.collection('user');
 
-  static Future<void> createUser() async {
+  static Future<String?> createUser() async {
     try {
-      await _userCollection.add({
-        'name': "No Name",
+      final newDoc = await _userCollection.add({
+        'name': "Noname",
         'image_path':
             "https://images.unsplash.com/photo-1472396961693-142e6e269027?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=MnwxNTgwfDB8MXxzZWFyY2h8Mjl8fE5hdHVyZXxlbnwwfHx8fDE2NzgwODY0NTY&ixlib=rb-4.0.3&q=80&w=400",
       });
       if (kDebugMode) {
         print("SUCCESS");
+        return newDoc.id;
       }
     } catch (e) {
       if (kDebugMode) {
         print("FAILED ===== $e");
+        return null;
       }
     }
+    return null;
   }
 
   static Future<List<QueryDocumentSnapshot>?> fetchUsers() async {
