00cb51b4d2731c076bf249899390903deac5c7e8
Ituking <77528963+Ituking@users.noreply.github.com>
2023-03-11 00:30:46 +0900

    userコレクションの情報を取得するように定義

--- lib/firestore/room_firestore.dart
+++ lib/firestore/room_firestore.dart
@@ -1,1 +1,7 @@
-class RoomFireStore {}
+import 'package:cloud_firestore/cloud_firestore.dart';
+
+class RoomFireStore {
+  static final FirebaseFirestore _firebaseFirestoreInstance =
+      FirebaseFirestore.instance;
+  static final _userCollection = _firebaseFirestoreInstance.collection('user');
+}
