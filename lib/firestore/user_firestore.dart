c4f05749d8dfef86a6361e19b596bfe8aced5d00
Ituking <77528963+Ituking@users.noreply.github.com>
2023-03-10 16:14:59 +0900

    Firestore インスタンスの作成

--- lib/firestore/user_firestore.dart
+++ lib/firestore/user_firestore.dart
@@ -1,1 +1,5 @@
-class UserFirestore {}
+import 'package:cloud_firestore/cloud_firestore.dart';
+
+class UserFirestore {
+  static FirebaseFirestore _firebaseFirestore = FirebaseFirestore.instance;
+}
