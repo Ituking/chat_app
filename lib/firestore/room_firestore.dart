1a28eb5b132ee3c483d21c8c12deb5ac7ec01f37
Ituking <77528963+Ituking@users.noreply.github.com>
2023-03-11 00:37:54 +0900

    他ユーザーとのトークルームを作成する処理の実装

--- lib/firestore/room_firestore.dart
+++ lib/firestore/room_firestore.dart
@@ -4,4 +4,6 @@ class RoomFireStore {
   static final FirebaseFirestore _firebaseFirestoreInstance =
       FirebaseFirestore.instance;
   static final _roomCollection = _firebaseFirestoreInstance.collection('room');
+
+  static Future<void> createRoom() async {}
 }
