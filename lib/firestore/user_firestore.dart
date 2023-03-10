99b797638c05aea76ae6f99f6a3719f81aa68001
Ituking <77528963+Ituking@users.noreply.github.com>
2023-03-10 23:30:48 +0900

    userコレクションにデータを入れる処理の実装

--- lib/firestore/user_firestore.dart
+++ lib/firestore/user_firestore.dart
@@ -1,4 +1,5 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:flutter/foundation.dart';
 
 class UserFirestore {
   static final FirebaseFirestore _firebaseFirestore =
@@ -6,6 +7,15 @@ class UserFirestore {
   static final _userCollection = _firebaseFirestoreInstance.collection('user');
 
   static Future<void> createUser() async {
-    try {} catch (e) {}
+    try {
+      await _userCollection.add({
+        'name': "No Name",
+        'image_path':
+            "https://images.unsplash.com/photo-1472396961693-142e6e269027?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=MnwxNTgwfDB8MXxzZWFyY2h8Mjl8fE5hdHVyZXxlbnwwfHx8fDE2NzgwODY0NTY&ixlib=rb-4.0.3&q=80&w=400",
+      });
+      if (kDebugMode) {
+        print("SUCCESS");
+      }
+    } catch (e) {}
   }
 }
