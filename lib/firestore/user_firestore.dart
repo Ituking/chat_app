75c98d86f73ca5279371d7c5ea86efcf28b9a84f
Ituking <77528963+Ituking@users.noreply.github.com>
2023-03-10 23:22:47 +0900

    エラーハンドリングの対応

--- lib/firestore/user_firestore.dart
+++ lib/firestore/user_firestore.dart
@@ -4,4 +4,8 @@ class UserFirestore {
   static final FirebaseFirestore _firebaseFirestore =
       FirebaseFirestore.instance;
   static final _userCollection = _firebaseFirestoreInstance.collection('user');
+
+  static Future<void> createUser() async {
+    try {} catch (e) {}
+  }
 }
