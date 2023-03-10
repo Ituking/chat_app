3e93a8d492c246cd8dadeeeb47bcfe25fcfa12a2
Ituking <77528963+Ituking@users.noreply.github.com>
2023-03-10 23:54:01 +0900

    アプリ起動時に新しいユーザーが作られるように実装

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:chat_app/firestore/user_firestore.dart';
 import 'package:chat_app/pages/top_page.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
@@ -9,6 +10,7 @@ void main() async {
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
   );
+  await UserFirestore.createUser();
   runApp(const MyApp());
 }
 
