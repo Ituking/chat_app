57a9afd5a8a7fa7d49306def97d7b9b8ab971592
Ituking <77528963+Ituking@users.noreply.github.com>
2023-03-16 23:58:11 +0900

    fetchMessageSnapshot()で取得した情報の表示をするように実装

--- lib/pages/talk_room_page.dart
+++ lib/pages/talk_room_page.dart
@@ -1,3 +1,5 @@
+import 'package:chat_app/firestore/room_firestore.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:intl/intl.dart' as intl;
 
@@ -21,54 +23,59 @@ class _TalkRoomPageState extends State<TalkRoomPage> {
         child: Stack(
           alignment: Alignment.bottomCenter,
           children: [
-            Padding(
-              padding: const EdgeInsets.only(bottom: 60),
-              child: ListView.builder(
-                  physics: const RangeMaintainingScrollPhysics(),
-                  shrinkWrap: true,
-                  reverse: true,
-                  itemCount: messageList.length,
-                  itemBuilder: (context, index) {
-                    return Padding(
-                      padding: EdgeInsets.only(
-                          top: 10,
-                          left: 10,
-                          right: 10,
-                          bottom: index == 0 ? 10 : 0),
-                      child: Row(
-                        crossAxisAlignment: CrossAxisAlignment.end,
-                        textDirection: messageList[index].isMe
-                            ? TextDirection.rtl
-                            : TextDirection.ltr,
-                        children: [
-                          Container(
-                            constraints: BoxConstraints(
-                                maxWidth:
-                                    MediaQuery.of(context).size.width * 0.6),
-                            decoration: BoxDecoration(
-                              color: messageList[index].isMe
-                                  ? Colors.blueGrey
-                                  : Colors.green,
-                              borderRadius: BorderRadius.circular(15),
+            StreamBuilder<QuerySnapshot>(
+                stream: RoomFireStore.fetchMessageSnapshot(roomId),
+                builder: (context, snapshot) {
+                  return Padding(
+                    padding: const EdgeInsets.only(bottom: 60),
+                    child: ListView.builder(
+                        physics: const RangeMaintainingScrollPhysics(),
+                        shrinkWrap: true,
+                        reverse: true,
+                        itemCount: messageList.length,
+                        itemBuilder: (context, index) {
+                          return Padding(
+                            padding: EdgeInsets.only(
+                                top: 10,
+                                left: 10,
+                                right: 10,
+                                bottom: index == 0 ? 10 : 0),
+                            child: Row(
+                              crossAxisAlignment: CrossAxisAlignment.end,
+                              textDirection: messageList[index].isMe
+                                  ? TextDirection.rtl
+                                  : TextDirection.ltr,
+                              children: [
+                                Container(
+                                  constraints: BoxConstraints(
+                                      maxWidth:
+                                          MediaQuery.of(context).size.width *
+                                              0.6),
+                                  decoration: BoxDecoration(
+                                    color: messageList[index].isMe
+                                        ? Colors.blueGrey
+                                        : Colors.green,
+                                    borderRadius: BorderRadius.circular(15),
+                                  ),
+                                  padding: const EdgeInsets.symmetric(
+                                      horizontal: 10, vertical: 6),
+                                  child: Text(
+                                    messageList[index].message,
+                                    style: TextStyle(
+                                      color: messageList[index].isMe
+                                          ? Colors.white
+                                          : Colors.white,
+                                    ),
+                                  ),
+                                ),
+                                Text(intl.DateFormat('HH:mm')
+                                    .format(messageList[index].sendTime)),
+                              ],
                             ),
-                            padding: const EdgeInsets.symmetric(
-                                horizontal: 10, vertical: 6),
-                            child: Text(
-                              messageList[index].message,
-                              style: TextStyle(
-                                color: messageList[index].isMe
-                                    ? Colors.white
-                                    : Colors.white,
-                              ),
-                            ),
-                          ),
-                          Text(intl.DateFormat('HH:mm')
-                              .format(messageList[index].sendTime)),
-                        ],
-                      ),
-                    );
-                  }),
-            ),
+                          );
+                        }),
+                  );
+                }),
             Column(
               mainAxisSize: MainAxisSize.min,
               children: [
