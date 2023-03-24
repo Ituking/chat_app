1e95e6cc5a1a5d8ca4fee14c480e078146f77aec
Ituking <77528963+Ituking@users.noreply.github.com>
2023-03-25 01:36:06 +0900

    選択した画像をアップロードする処理の実装

--- lib/pages/setting_profile_page.dart
+++ lib/pages/setting_profile_page.dart
@@ -39,6 +39,8 @@ class _SettingProfilePageState extends State<SettingProfilePage> {
     }
   }
 
+  Future<void> uploadImage() async {}
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
