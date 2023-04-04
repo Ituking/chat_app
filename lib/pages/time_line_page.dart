0a2cc251b897640ffb9e192f4115b95f0ad4f848
Ituking <77528963+Ituking@users.noreply.github.com>
2023-04-04 23:22:15 +0900

    time_line_page.dartの作成

---
+++ lib/pages/time_line_page.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/src/widgets/framework.dart';
+import 'package:flutter/src/widgets/placeholder.dart';
+
+class TimeLinePage extends StatefulWidget {
+  const TimeLinePage({super.key});
+
+  @override
+  State<TimeLinePage> createState() => _TimeLinePageState();
+}
+
+class _TimeLinePageState extends State<TimeLinePage> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
