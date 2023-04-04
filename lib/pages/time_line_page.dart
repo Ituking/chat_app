fdd2ad557c8717c963bdc64849a8cced33ecd315
Ituking <77528963+Ituking@users.noreply.github.com>
2023-04-04 23:55:36 +0900

    postListの定義

--- lib/pages/time_line_page.dart
+++ lib/pages/time_line_page.dart
@@ -1,3 +1,4 @@
+import 'package:chat_app/model/post.dart';
 import 'package:flutter/material.dart';
 
 class TimeLinePage extends StatefulWidget {
@@ -8,6 +9,23 @@ class TimeLinePage extends StatefulWidget {
 }
 
 class _TimeLinePageState extends State<TimeLinePage> {
+  List<Post> postList = [
+    Post(
+      id: "0001",
+      postAccountId: "0001",
+      postTime: DateTime.now(),
+      imagePath:
+          "https://images.unsplash.com/photo-1471922694854-ff1b63b20054?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1472&q=80",
+    ),
+    Post(
+      id: "0002",
+      postAccountId: "0002",
+      postTime: DateTime.now(),
+      imagePath:
+          "https://images.unsplash.com/photo-1542233637-20456b09d882?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=774&q=80",
+    ),
+  ];
+
   @override
   Widget build(BuildContext context) {
     return const Scaffold(
