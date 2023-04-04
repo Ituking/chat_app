7c2ed80630122b547f7e2d2966084d8c0df64ec5
Ituking <77528963+Ituking@users.noreply.github.com>
2023-04-04 23:35:10 +0900

    post.dartの作成

---
+++ lib/model/post.dart
@@ -0,0 +1,13 @@
+class Post {
+  String id;
+  String content;
+  String postAccountId;
+  DateTime? postTime;
+
+  Post({
+    required this.id,
+    required this.content,
+    required this.postAccountId,
+    required this.postTime,
+  });
+}
