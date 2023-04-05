742a14a64ea1ef175fecca0290530572768926d8
Ituking <77528963+Ituking@users.noreply.github.com>
2023-04-05 21:09:55 +0900

    ユーザーアイコンの実装（ホームページ）

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,5 +1,6 @@
 import 'package:chat_app/model/post.dart';
 import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
@@ -31,16 +32,10 @@ class _HomePageState extends State<HomePage> {
           return Padding(
             padding: const EdgeInsets.all(8.0),
             child: Row(
-              children: const [
+              children: [
                 CircleAvatar(
                   radius: 22,
-                  foregroundImage: NetworkImage(
-                    "https://images.unsplash.com/photo-1471922694854-ff1b63b20054?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1472&q=80",
-                  ),
-                ),
-                Padding(
-                  padding: EdgeInsets.all(8.0),
-                  child: Text("Emma"),
+                  foregroundImage: NetworkImage(userPosts[index].imagePath!),
                 ),
               ],
             ),
