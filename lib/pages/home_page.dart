6a4c164468ca16ef3f0b350f196e3037bc1e99bb
Ituking <77528963+Ituking@users.noreply.github.com>
2023-04-05 20:02:01 +0900

    変数UserPostsの定義

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,3 +1,4 @@
+import 'package:chat_app/model/post.dart';
 import 'package:flutter/material.dart';
 
 class HomePage extends StatefulWidget {
@@ -8,6 +9,15 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  List<Post> UserPosts = [
+    Post(
+      id: "0001",
+      postAccountId: "0001",
+      postTime: DateTime.now(),
+      imagePath:
+          "https://images.unsplash.com/photo-1471922694854-ff1b63b20054?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1472&q=80",
+    ),
+  ];
   @override
   Widget build(BuildContext context) {
     return Scaffold(
