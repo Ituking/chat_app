2165e1daa7a747708607bf3d748f3a97cbab45a1
Ituking <77528963+Ituking@users.noreply.github.com>
2023-04-05 21:45:11 +0900

    テストユーザーの追加（ホームページ）

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -12,7 +12,14 @@ class HomePage extends StatefulWidget {
 
 class _HomePageState extends State<HomePage> {
   List<User> postedUser = [
-    User(name: "Emma", uid: "0001"),
+    User(
+      name: "Emma",
+      uid: "0001",
+    ),
+    User(
+      name: "John",
+      uid: "0002",
+    ),
   ];
   List<Post> userPosts = [
     Post(
@@ -21,6 +28,13 @@ class _HomePageState extends State<HomePage> {
       postTime: DateTime.now(),
       imagePath:
           "https://images.unsplash.com/photo-1471922694854-ff1b63b20054?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1472&q=80",
+    ),
+    Post(
+      id: "0002",
+      postAccountId: "0002",
+      postTime: DateTime.now(),
+      imagePath:
+          "https://images.unsplash.com/photo-1542233637-20456b09d882?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=774&q=80",
     ),
   ];
   @override
