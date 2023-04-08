197074eda7e1e55945c09facffd763cbf6b6f0f8
Ituking <77528963+Ituking@users.noreply.github.com>
2023-04-08 14:26:23 +0900

    ユーザー名の実装（ホームページ）

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -53,6 +53,7 @@ class _HomePageState extends State<HomePage> {
               children: const [
                 ListTile(
                   leading: CircleAvatar(),
+                  title: Text("Emma"),
                 ),
               ],
             ),
