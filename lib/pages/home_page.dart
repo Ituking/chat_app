113dcc6e4cd466a80960c0db87eec64bf647f271
Ituking <77528963+Ituking@users.noreply.github.com>
2023-04-10 10:37:45 +0900

    ユーザー名と投稿日時のスタイルの設定（ホームページ）

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -73,8 +73,18 @@ class _HomePageState extends State<HomePage> {
                         "https://images.unsplash.com/photo-1472396961693-142e6e269027?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=MnwxNTgwfDB8MXxzZWFyY2h8Mjl8fE5hdHVyZXxlbnwwfHx8fDE2NzgwODY0NTY&ixlib=rb-4.0.3&q=80&w=400",
                       ),
                     ),
-                    title: Text(user.name),
-                    subtitle: Text(formattedDate),
+                    title: Text(
+                      user.name,
+                      style: const TextStyle(
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                    subtitle: Text(
+                      formattedDate,
+                      style: const TextStyle(
+                        fontWeight: FontWeight.normal,
+                      ),
+                    ),
                   ),
                   Expanded(
                     child: Container(
