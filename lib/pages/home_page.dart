8e91c5e94a61610c269d60665a4b2a216de37320
Ituking <77528963+Ituking@users.noreply.github.com>
2023-04-10 09:58:46 +0900

    userPostsListに格納された分だけ表示されるように実装（ホームページ）

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -52,6 +52,94 @@ class _HomePageState extends State<HomePage> {
               postedUser.firstWhere((user) => user.uid == post.postAccountId);
           final formattedDate =
               DateFormat('EEE MMM dd yyyy HH:mm').format(post.postTime!);
+          return Card(
+            child: SizedBox(
+              height: 350,
+              child: Column(
+                children: [
+                  ListTile(
+                    leading: const CircleAvatar(),
+                    title: Text(user.name),
+                    subtitle: Text(formattedDate),
+                  ),
+                  Expanded(
+                    child: Container(
+                      decoration: BoxDecoration(
+                        image: DecorationImage(
+                          image: NetworkImage(
+                            post.imagePath!,
+                          ),
+                          fit: BoxFit.cover,
+                        ),
+                      ),
+                    ),
+                  ),
+                  const SizedBox(
+                    height: 14.0,
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                    children: [
+                      Row(
+                        children: const [
+                          Icon(
+                            Icons.thumb_up,
+                            color: Colors.grey,
+                          ),
+                          SizedBox(
+                            width: 8.0,
+                          ),
+                          Text(
+                            "Like",
+                            style: TextStyle(
+                              color: Colors.grey,
+                            ),
+                          ),
+                        ],
+                      ),
+                      Row(
+                        children: const [
+                          Icon(
+                            Icons.comment,
+                            color: Colors.grey,
+                          ),
+                          SizedBox(
+                            width: 8.0,
+                          ),
+                          Text(
+                            "Comments",
+                            style: TextStyle(
+                              color: Colors.grey,
+                            ),
+                          ),
+                        ],
+                      ),
+                      Row(
+                        children: const [
+                          Icon(
+                            Icons.share,
+                            color: Colors.grey,
+                          ),
+                          SizedBox(
+                            width: 8.0,
+                          ),
+                          Text(
+                            "Share",
+                            style: TextStyle(
+                              color: Colors.grey,
+                            ),
+                          ),
+                        ],
+                      ),
+                    ],
+                  ),
+                  const SizedBox(
+                    height: 12.0,
+                  ),
+                ],
+              ),
+            ),
+          );
         },
       ),
     );
