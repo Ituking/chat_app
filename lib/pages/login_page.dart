0d5870db9798b108cdf02500b8f59566ab77089d
Ituking <77528963+Ituking@users.noreply.github.com>
2023-04-11 21:44:50 +0900

    Chat Appの上の余白の設定（ログインページ）

--- lib/pages/login_page.dart
+++ lib/pages/login_page.dart
@@ -20,7 +20,7 @@ class _LoginPageState extends State<LoginPage> {
           child: Column(
             children: [
               SizedBox(
-                height: MediaQuery.of(context).size.height * 0.4,
+                height: MediaQuery.of(context).size.height * 0.35,
               ),
               const Text(
                 "Chat App",
