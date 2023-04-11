4adde9223c01c5ba24b45fcc37dc942c776af679
Ituking <77528963+Ituking@users.noreply.github.com>
2023-04-11 21:14:04 +0900

    メールアドレスとパスワードのコントローラーの定義

--- lib/pages/login_page.dart
+++ lib/pages/login_page.dart
@@ -8,6 +8,9 @@ class LoginPage extends StatefulWidget {
 }
 
 class _LoginPageState extends State<LoginPage> {
+  TextEditingController emailController = TextEditingController();
+  TextEditingController passwordController = TextEditingController();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
