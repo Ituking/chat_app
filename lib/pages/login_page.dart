08171871d3d4e9f500973439d185ba16db3b3cc2
Ituking <77528963+Ituking@users.noreply.github.com>
2023-04-11 21:05:20 +0900

    ChatAppの表示

--- lib/pages/login_page.dart
+++ lib/pages/login_page.dart
@@ -10,6 +10,14 @@ class LoginPage extends StatefulWidget {
 class _LoginPageState extends State<LoginPage> {
   @override
   Widget build(BuildContext context) {
-    return const Scaffold();
+    return Scaffold(
+      body: SafeArea(
+        child: Column(
+          children: const [
+            Text("ChatApp"),
+          ],
+        ),
+      ),
+    );
   }
 }
