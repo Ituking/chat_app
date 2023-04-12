c45f71267ab1ca1c591de212766d59df7b50512d
Ituking <77528963+Ituking@users.noreply.github.com>
2023-04-13 06:59:18 +0900

    Sign Upの表示

--- lib/pages/create_account_page.dart
+++ lib/pages/create_account_page.dart
@@ -10,6 +10,11 @@ class CreateAccountPage extends StatefulWidget {
 class _CreateAccountPageState extends State<CreateAccountPage> {
   @override
   Widget build(BuildContext context) {
-    return const Scaffold();
+    return Scaffold(
+      appBar: AppBar(),
+      body: const Center(
+        child: Text("Sign Up"),
+      ),
+    );
   }
 }
