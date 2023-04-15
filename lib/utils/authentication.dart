8d8419021eb4c4587c104d71bc6d08c91f31e63c
Ituking <77528963+Ituking@users.noreply.github.com>
2023-04-15 21:56:04 +0900

    変数の定義

--- lib/utils/authentication.dart
+++ lib/utils/authentication.dart
@@ -1,1 +1,6 @@
-class Authentication {}
+import 'package:firebase_auth/firebase_auth.dart';
+
+class Authentication {
+  static final FirebaseAuth _firebaseAuth = FirebaseAuth.instance;
+  static User? currentFirebaseUser;
+}
