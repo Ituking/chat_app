3dedbb669238085299a94806765bcfaad990f6dd
Ituking <77528963+Ituking@users.noreply.github.com>
2023-04-16 17:47:13 +0900

    ユーザー情報をFirebaseに保存する処理が成功した場合の処理の実装

--- lib/firestore/account_firestore.dart
+++ lib/firestore/account_firestore.dart
@@ -1,5 +1,6 @@
 import 'package:chat_app/model/account.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:flutter/foundation.dart';
 
 class AccountFirestore {
   static final _firestoreInstance = FirebaseFirestore.instance;
@@ -7,6 +8,19 @@ class AccountFirestore {
       _firestoreInstance.collection('account');
 
   static Future<dynamic> setUser(Account newAccount) async {
-    try {} on FirebaseException catch (e) {}
+    try {
+      await account.doc(newAccount.id).set({
+        'name': newAccount.name,
+        'user_id': newAccount.userId,
+        'self_introduction': newAccount.selfIntroduction,
+        'image_path': newAccount.imagePath,
+        'created_time': Timestamp.now(),
+        'updated_time': Timestamp.now(),
+      });
+      if (kDebugMode) {
+        print("Account registration completed.");
+      }
+      return true;
+    } on FirebaseException catch (e) {}
   }
 }
