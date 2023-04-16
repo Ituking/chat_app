176a033c96fc4ec032a5ec3631de72297a80c5a4
Ituking <77528963+Ituking@users.noreply.github.com>
2023-04-16 17:17:37 +0900

    Accountクラスの定義

--- lib/model/account.dart
+++ lib/model/account.dart
@@ -1,1 +1,9 @@
-class Account {}
+class Account {
+  String id;
+  String name;
+  String imagePath;
+  String selfIntroduction;
+  String userId;
+  DateTime? createdTime;
+  DateTime? updatedTime;
+}
