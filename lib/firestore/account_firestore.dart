c744c5bb84be88acc3ac04d7bf531142a32fad0b
Ituking <77528963+Ituking@users.noreply.github.com>
2023-04-16 17:34:54 +0900

    ユーザー情報をFirebaseに保存する処理の実装

--- lib/firestore/account_firestore.dart
+++ lib/firestore/account_firestore.dart
@@ -1,7 +1,10 @@
+import 'package:chat_app/model/account.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 
 class AccountFirestore {
   static final _firestoreInstance = FirebaseFirestore.instance;
   static final CollectionReference account =
       _firestoreInstance.collection('account');
+
+  static Future<dynamic> setUser(Account newAccount) async {}
 }
