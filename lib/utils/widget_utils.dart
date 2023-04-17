b6aa45ab75759fc38213d1f17a8b65697dee4727
Ituking <77528963+Ituking@users.noreply.github.com>
2023-04-17 20:06:20 +0900

    widget_utils.dartの作成

--- lib/utils/widget_utils.dart
+++ lib/utils/widget_utils.dart
@@ -1,1 +1,20 @@
-class WidgetUtils {}
+import 'package:flutter/material.dart';
+
+class WidgetUtils {
+  static AppBar createAppBar(String title) {
+    return AppBar(
+      backgroundColor: Colors.transparent,
+      elevation: 0,
+      iconTheme: const IconThemeData(
+        color: Colors.black,
+      ),
+      title: Text(
+        title,
+        style: const TextStyle(
+          fontWeight: FontWeight.bold,
+          fontSize: 28,
+        ),
+      ),
+    );
+  }
+}
