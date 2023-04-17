9999900606f6c934c374ae64e44674d9d45874eb
Ituking <77528963+Ituking@users.noreply.github.com>
2023-04-17 18:17:56 +0900

    post_widget.dartの作成

---
+++ lib/post_widget.dart
@@ -0,0 +1,15 @@
+import 'package:chat_app/model/post.dart';
+import 'package:flutter/material.dart';
+
+class PostWidget extends StatelessWidget {
+  final Post post;
+  const PostWidget({super.key, required this.post});
+
+  @override
+  Widget build(BuildContext context) {
+    return Image.network(
+      post.imagePath!,
+      fit: BoxFit.cover,
+    );
+  }
+}
