9b19e7bc50bcc221fb21a73b2fcc288efe4fdee3
Ituking <77528963+Ituking@users.noreply.github.com>
2023-04-17 19:50:50 +0900

    edit_account_page.dartの作成

---
+++ lib/pages/edit_account_page.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class EditAccountPage extends StatefulWidget {
+  const EditAccountPage({super.key});
+
+  @override
+  State<EditAccountPage> createState() => _EditAccountPageState();
+}
+
+class _EditAccountPageState extends State<EditAccountPage> {
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold();
+  }
+}
