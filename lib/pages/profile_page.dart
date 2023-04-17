3759a7f07135b4bf375c8ef91ff743d15167c955
Ituking <77528963+Ituking@users.noreply.github.com>
2023-04-17 16:53:30 +0900

    AppBarの実装（プロフィールページ）

--- lib/pages/profile_page.dart
+++ lib/pages/profile_page.dart
@@ -12,67 +12,21 @@ class _ProfilePageState extends State<ProfilePage> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        backgroundColor: Colors.transparent,
+        backgroundColor: Colors.white,
         elevation: 0,
-        centerTitle: true,
-        automaticallyImplyLeading: false,
         title: const Text(
-          "Profile",
+          'Profile',
           style: TextStyle(
             color: Colors.black,
+            fontSize: 28.0,
             fontWeight: FontWeight.bold,
           ),
         ),
-      ),
-      body: Column(
-        children: [
-          Container(
-            color: Colors.red.withOpacity(0.3),
-            height: MediaQuery.of(context).size.height * 0.25,
-            child: Column(
-              children: [
-                Padding(
-                  padding: const EdgeInsets.all(
-                    8.0,
-                  ),
-                  child: Row(
-                    children: [
-                      const CircleAvatar(
-                        radius: 40,
-                        foregroundImage: NetworkImage(
-                          "https://images.unsplash.com/photo-1472396961693-142e6e269027?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=MnwxNTgwfDB8MXxzZWFyY2h8Mjl8fE5hdHVyZXxlbnwwfHx8fDE2NzgwODY0NTY&ixlib=rb-4.0.3&q=80&w=400",
-                        ),
-                      ),
-                      const SizedBox(
-                        width: 10,
-                      ),
-                      Column(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: const [
-                          Text(
-                            "John",
-                            style: TextStyle(
-                              fontSize: 20,
-                              fontWeight: FontWeight.bold,
-                            ),
-                          ),
-                          SizedBox(
-                            height: 5,
-                          ),
-                          Text(
-                            "0001",
-                            style: TextStyle(
-                              color: Colors.grey,
-                              fontWeight: FontWeight.normal,
-                            ),
-                          ),
-                        ],
-                      ),
-                    ],
-                  ),
-                ),
-              ],
-            ),
+        actions: [
+          IconButton(
+            icon: const Icon(Icons.more_horiz),
+            color: Colors.grey,
+            onPressed: () {},
           ),
         ],
       ),
