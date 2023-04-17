e2fb49ebb3f15086be60994fcb0ef435df531ab8
Ituking <77528963+Ituking@users.noreply.github.com>
2023-04-17 17:53:32 +0900

    ユーザーのこれまでの投稿を実装（プロフィールページ）

--- lib/pages/profile_page.dart
+++ lib/pages/profile_page.dart
@@ -1,3 +1,4 @@
+import 'package:chat_app/model/post.dart';
 import 'package:flutter/material.dart';
 
 class ProfilePage extends StatefulWidget {
@@ -8,6 +9,23 @@ class ProfilePage extends StatefulWidget {
 }
 
 class _ProfilePageState extends State<ProfilePage> {
+  List<Post> postList = [
+    Post(
+      id: "0001",
+      postAccountId: "0001",
+      postTime: DateTime.now(),
+      imagePath:
+          "https://images.unsplash.com/photo-1471922694854-ff1b63b20054?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1472&q=80",
+    ),
+    Post(
+      id: "0002",
+      postAccountId: "0002",
+      postTime: DateTime.now(),
+      imagePath:
+          "https://images.unsplash.com/photo-1542233637-20456b09d882?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=774&q=80",
+    ),
+  ];
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -48,43 +66,64 @@ class _ProfilePageState extends State<ProfilePage> {
               padding: const EdgeInsets.fromLTRB(16.0, 140.0, 16.0, 0),
               child: Column(
                 crossAxisAlignment: CrossAxisAlignment.start,
-                children: const [
-                  CircleAvatar(
+                children: [
+                  const CircleAvatar(
                     radius: 50.0,
                     backgroundImage: NetworkImage(
                       'https://cdn.pixabay.com/photo/2015/04/19/08/32/marguerite-729510__480.jpg',
                     ),
                   ),
-                  SizedBox(height: 10.0),
-                  Text(
+                  const SizedBox(height: 10.0),
+                  const Text(
                     'Jane Doe',
                     style: TextStyle(
                       fontSize: 24.0,
                       fontWeight: FontWeight.bold,
                     ),
                   ),
-                  SizedBox(height: 8.0),
-                  Text(
+                  const SizedBox(height: 8.0),
+                  const Text(
                     'Software Engineer',
                     style: TextStyle(
                       color: Colors.grey,
                       fontSize: 16.0,
                     ),
                   ),
-                  SizedBox(height: 16.0),
-                  Text(
+                  const SizedBox(height: 16.0),
+                  const Text(
                     'About',
                     style: TextStyle(
                       fontSize: 20.0,
                       fontWeight: FontWeight.bold,
                     ),
                   ),
-                  SizedBox(height: 8.0),
-                  Text(
+                  const SizedBox(height: 8.0),
+                  const Text(
                     'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum eget ullamcorper est, id pretium nisi. Aliquam vel malesuada magna, eu consectetur sapien. Aliquam consequat felis at nulla lacinia, ut vestibulum urna sollicitudin. Sed fermentum, tellus sed volutpat imperdiet, dolor lectus accumsan nulla, sit amet pharetra metus nisi id purus. Nunc eu lectus dignissim, blandit magna id, bibendum ex. Nam sed elit quam. Aliquam eu leo nulla. Etiam iaculis velit sit amet nunc posuere, vel interdum sapien maximus. Sed porttitor mi mauris, sed vestibulum mauris lacinia vel. Integer vitae quam interdum, commodo tellus non, tincidunt odio. Praesent vel urna ut justo sodales ullamcorper.',
                     style: TextStyle(
                       fontSize: 16.0,
                     ),
+                  ),
+                  GridView.builder(
+                    shrinkWrap: true,
+                    physics: const NeverScrollableScrollPhysics(),
+                    itemCount: postList.length,
+                    gridDelegate:
+                        const SliverGridDelegateWithFixedCrossAxisCount(
+                      crossAxisCount: 3,
+                    ),
+                    itemBuilder: (BuildContext context, int index) {
+                      return Container(
+                        margin: const EdgeInsets.all(2.0),
+                        decoration: BoxDecoration(
+                          image: DecorationImage(
+                            image: NetworkImage(postList[index].imagePath!),
+                            fit: BoxFit.cover,
+                          ),
+                          borderRadius: BorderRadius.circular(10.0),
+                        ),
+                      );
+                    },
                   ),
                 ],
               ),
