a82f1d4fb6b4712f83e83ec8bce6628e4d4c5881
Ituking <77528963+Ituking@users.noreply.github.com>
2023-04-17 16:55:03 +0900

    ヘッダーの実装（プロフィールページ）

--- lib/pages/profile_page.dart
+++ lib/pages/profile_page.dart
@@ -30,6 +30,23 @@ class _ProfilePageState extends State<ProfilePage> {
           ),
         ],
       ),
+      body: SingleChildScrollView(
+        child: Stack(
+          children: [
+            Container(
+              height: 200.0,
+              decoration: const BoxDecoration(
+                image: DecorationImage(
+                  image: NetworkImage(
+                    'https://cdn.pixabay.com/photo/2015/04/23/22/00/tree-736885__480.jpg',
+                  ),
+                  fit: BoxFit.cover,
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
