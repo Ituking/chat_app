bcdbd48881e60a569a9e79bb196756c8ce79cdc4
Ituking <77528963+Ituking@users.noreply.github.com>
2023-04-17 17:06:37 +0900

    余白の追加（プロフィールページ）

--- lib/pages/profile_page.dart
+++ lib/pages/profile_page.dart
@@ -79,6 +79,7 @@ class _ProfilePageState extends State<ProfilePage> {
                       fontWeight: FontWeight.bold,
                     ),
                   ),
+                  SizedBox(height: 8.0),
                 ],
               ),
             ),
