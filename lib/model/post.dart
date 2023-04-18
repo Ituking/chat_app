d4c99cc9720e1d58aaf1a3a713c1158c14338f63
Ituking <77528963+Ituking@users.noreply.github.com>
2023-04-18 23:25:41 +0900

    変数contentの定義

--- lib/model/post.dart
+++ lib/model/post.dart
@@ -1,12 +1,14 @@
 class Post {
   String id;
   String? imagePath;
+  String content;
   String postAccountId;
   DateTime? postTime;
 
   Post({
     required this.id,
-    this.imagePath,
+    required this.imagePath,
+    required this.content,
     required this.postAccountId,
     required this.postTime,
   });
