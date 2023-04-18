03aff0688e51c7690cc4a9fe3a1d79d62f83d5f7
Ituking <77528963+Ituking@users.noreply.github.com>
2023-04-18 23:05:17 +0900

    変数postsの定義

--- lib/firestore/post_firestore.dart
+++ lib/firestore/post_firestore.dart
@@ -2,4 +2,6 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 
 class PostFirestore {
   static final _firestoreInstance = FirebaseFirestore.instance;
+  static final CollectionReference posts =
+      _firestoreInstance.collection('posts');
 }
