da12cf2001840614c7762bfbadfb3293496d1874
Ituking <77528963+Ituking@users.noreply.github.com>
2023-04-19 00:11:38 +0900

    投稿内容の追加（タイムラインページ）

--- lib/pages/time_line_page.dart
+++ lib/pages/time_line_page.dart
@@ -19,7 +19,7 @@ class _TimeLinePageState extends State<TimeLinePage> {
       postTime: DateTime.now(),
       imagePath:
           "https://images.unsplash.com/photo-1471922694854-ff1b63b20054?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1472&q=80",
-      content: '',
+      content: 'On a sunny day, you can still go to the beach 🌊.',
     ),
     Post(
       id: "0002",
@@ -27,7 +27,7 @@ class _TimeLinePageState extends State<TimeLinePage> {
       postTime: DateTime.now(),
       imagePath:
           "https://images.unsplash.com/photo-1542233637-20456b09d882?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=774&q=80",
-      content: '',
+      content: 'A day healed by nature 🌳.',
     ),
   ];
 
