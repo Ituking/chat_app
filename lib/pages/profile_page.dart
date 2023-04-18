b628ee9b588b9a272794a1bf37e3e4016d77ebca
Ituking <77528963+Ituking@users.noreply.github.com>
2023-04-19 00:12:24 +0900

    投稿内容の追加（プロフィールページ）

--- lib/pages/profile_page.dart
+++ lib/pages/profile_page.dart
@@ -20,6 +20,7 @@ class _ProfilePageState extends State<ProfilePage> {
       postTime: DateTime.now(),
       imagePath:
           "https://images.unsplash.com/photo-1471922694854-ff1b63b20054?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1472&q=80",
+      content: '',
     ),
     Post(
       id: "0002",
@@ -27,6 +28,7 @@ class _ProfilePageState extends State<ProfilePage> {
       postTime: DateTime.now(),
       imagePath:
           "https://images.unsplash.com/photo-1542233637-20456b09d882?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=774&q=80",
+      content: '',
     ),
   ];
 
