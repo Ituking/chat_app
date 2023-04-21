d2d384da416e8e12cbf17ba60b688144c612c194
Ituking <77528963+Ituking@users.noreply.github.com>
2023-04-21 22:55:39 +0900

    写真選択ボタンの実装（投稿ページ）

--- lib/pages/post_page.dart
+++ lib/pages/post_page.dart
@@ -56,6 +56,11 @@ class _PostPageState extends State<PostPage> {
                 cursorColor: Colors.black,
               ),
             ),
+            ListTile(
+              leading: const Icon(Icons.photo_library),
+              title: const Text("Add a photo"),
+              onTap: () {},
+            ),
           ],
         ),
       ),
