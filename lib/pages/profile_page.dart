f3d445efc7441a34f9bde3de2cd623c0023c17dd
Ituking <77528963+Ituking@users.noreply.github.com>
2023-04-25 06:56:52 +0900

    いらないものの削除

--- lib/pages/profile_page.dart
+++ lib/pages/profile_page.dart
@@ -14,24 +14,6 @@ class ProfilePage extends StatefulWidget {
 
 class _ProfilePageState extends State<ProfilePage> {
   Account myAccount = Authentication.myAccount!;
-  List<Post> postList = [
-    Post(
-      id: "0001",
-      postAccountId: "0001",
-      postTime: Timestamp.now(),
-      imagePath:
-          "https://images.unsplash.com/photo-1471922694854-ff1b63b20054?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1472&q=80",
-      content: '',
-    ),
-    Post(
-      id: "0002",
-      postAccountId: "0002",
-      postTime: Timestamp.now(),
-      imagePath:
-          "https://images.unsplash.com/photo-1542233637-20456b09d882?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=774&q=80",
-      content: '',
-    ),
-  ];
 
   @override
   Widget build(BuildContext context) {
