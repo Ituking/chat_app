c1d61b0183641f9b12600be45cfaf6420705b999
Ituking <77528963+Ituking@users.noreply.github.com>
2023-05-07 17:16:19 +0900

    chat_input_field.dartの作成

---
+++ lib/utils/chat_input_field.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class ChatInputField extends StatelessWidget {
+  const ChatInputField({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
