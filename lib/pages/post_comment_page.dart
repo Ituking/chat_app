908c7463ca16e61332ccac9694ca257a2c46d36e
Ituking <77528963+Ituking@users.noreply.github.com>
2023-05-09 17:06:23 +0900

    post_comment_page.dartの作成

---
+++ lib/pages/post_comment_page.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class PostCommentPage extends StatefulWidget {
+  const PostCommentPage({super.key});
+
+  @override
+  State<PostCommentPage> createState() => _PostCommentPageState();
+}
+
+class _PostCommentPageState extends State<PostCommentPage> {
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold();
+  }
+}
