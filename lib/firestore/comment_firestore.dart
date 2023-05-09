831f1ab16b1c0425ce24abdab3d7344eb3a9d235
Ituking <77528963+Ituking@users.noreply.github.com>
2023-05-09 23:59:30 +0900

    変数commentsの定義
    commentsコレクションの情報を取得する処理を変数として定義

--- lib/firestore/comment_firestore.dart
+++ lib/firestore/comment_firestore.dart
@@ -2,4 +2,6 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 
 class CommentFirestore {
   static final _firestoreInstance = FirebaseFirestore.instance;
+  static final CollectionReference comments =
+      _firestoreInstance.collection('comments');
 }
