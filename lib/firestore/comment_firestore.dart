b9d6a3e86e334db694067754b95af70c736ff88a
Ituking <77528963+Ituking@users.noreply.github.com>
2023-05-10 02:00:52 +0900

    コメントが成功した場合の処理実装

--- lib/firestore/comment_firestore.dart
+++ lib/firestore/comment_firestore.dart
@@ -1,5 +1,6 @@
 import 'package:chat_app/model/comment.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:flutter/foundation.dart';
 
 class CommentFirestore {
   static final _firestoreInstance = FirebaseFirestore.instance;
@@ -21,6 +22,10 @@ class CommentFirestore {
         'comment_id': result.id,
         'comment_time': Timestamp.now(),
       });
+      if (kDebugMode) {
+        print("Comment Completed.");
+      }
+      return true;
     } on FirebaseException catch (e) {}
   }
 }
