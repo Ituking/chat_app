cc0643999707e7bf571cf42b6f02dceecd0bb8de
Ituking <77528963+Ituking@users.noreply.github.com>
2023-05-09 17:42:51 +0900

    AppBarの実装（コメントページ）

--- lib/pages/post_comment_page.dart
+++ lib/pages/post_comment_page.dart
@@ -1,3 +1,4 @@
+import 'package:chat_app/utils/widget_utils.dart';
 import 'package:flutter/material.dart';
 
 class PostCommentPage extends StatefulWidget {
@@ -11,9 +12,7 @@ class _PostCommentPageState extends State<PostCommentPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        elevation: 0,
-      ),
+      appBar: WidgetUtils.createAppBar("Comment"),
       body: const Center(
         child: Text("Comment"),
       ),
