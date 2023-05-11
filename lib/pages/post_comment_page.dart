15853921ff15dd17bfef851679458e6afa58658b
Ituking <77528963+Ituking@users.noreply.github.com>
2023-05-11 21:27:17 +0900

    いらないものの削除

--- lib/pages/post_comment_page.dart
+++ lib/pages/post_comment_page.dart
@@ -15,31 +15,6 @@ class PostCommentPage extends StatefulWidget {
 }
 
 class _PostCommentPageState extends State<PostCommentPage> {
-  Account commentAccount = Account(
-    id: "1",
-    name: "Emma",
-    imagePath:
-        "https://images.unsplash.com/photo-1472396961693-142e6e269027?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=MnwxNTgwfDB8MXxzZWFyY2h8Mjl8fE5hdHVyZXxlbnwwfHx8fDE2NzgwODY0NTY&ixlib=rb-4.0.3&q=80&w=400",
-    selfIntroduction:
-        "Hi everyone! I'm Emma. I'm passionate about Programming, and love sharing my experiences and thoughts with others.",
-    userId: "0001",
-  );
-
-  List<Comment> commentList = [
-    Comment(
-      id: "1",
-      content: "Beautiful photo! You're such a talented photographer.",
-      commentAccountId: "1",
-      commentTime: Timestamp.now(),
-    ),
-    Comment(
-      id: "2",
-      content: "This place looks amazing! I need to add it to my travel list.",
-      commentAccountId: "2",
-      commentTime: Timestamp.now(),
-    ),
-  ];
-
   TextEditingController contentController = TextEditingController();
 
   late final bottomSpace = MediaQuery.of(context).viewInsets.bottom;
