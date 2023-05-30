7b2a9eb8423f93985cb282ffcf60e8841b271448
Ituking <77528963+Ituking@users.noreply.github.com>
2023-05-30 22:11:55 +0900

    share_button.dartの作成

---
+++ lib/utils/share_button.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class ShareButton extends StatefulWidget {
+  const ShareButton({super.key});
+
+  @override
+  State<ShareButton> createState() => _ShareButtonState();
+}
+
+class _ShareButtonState extends State<ShareButton> {
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold();
+  }
+}
