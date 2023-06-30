9adf2847f948ae4b0100729b9edc2a3aa8633f89
Ituking <77528963+Ituking@users.noreply.github.com>
2023-06-30 14:24:17 +0900

    余白の設定（ユーザープロフィールページ）

--- lib/pages/user_profile_page.dart
+++ lib/pages/user_profile_page.dart
@@ -73,6 +73,9 @@ class _UserProfilePageState extends State<UserProfilePage> {
                       ),
                     ],
                   ),
+                  const SizedBox(
+                    height: 10.0,
+                  ),
                 ],
               ),
             ),
