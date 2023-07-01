1370c20af72e8136bf4d69996018ca2ce721f346
Ituking <77528963+Ituking@users.noreply.github.com>
2023-07-01 23:47:31 +0900

    bodyの実装（ユーザープロフィールページ）

--- lib/pages/user_profile_page.dart
+++ lib/pages/user_profile_page.dart
@@ -66,103 +66,7 @@ class _UserProfilePageState extends State<UserProfilePage> {
           ),
         ),
       ),
-      body: SingleChildScrollView(
-        child: Stack(
-          children: [
-            Container(
-              height: 200.0,
-              decoration: const BoxDecoration(
-                image: DecorationImage(
-                  image: NetworkImage(
-                    "https://cdn.pixabay.com/photo/2023/06/13/15/05/astronaut-8061095_1280.png",
-                  ),
-                  fit: BoxFit.cover,
-                ),
-              ),
-            ),
-            Padding(
-              padding: const EdgeInsets.fromLTRB(16.0, 140.0, 16.0, 0),
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    children: [
-                      CircleAvatar(
-                        radius: 50.0,
-                        backgroundColor: CupertinoColors.white,
-                        backgroundImage: const NetworkImage(
-                          "https://cdn.pixabay.com/photo/2023/06/13/15/05/astronaut-8061095_1280.png",
-                        ),
-                        child: Container(
-                          padding: const EdgeInsets.all(2),
-                          decoration: BoxDecoration(
-                            color: CupertinoColors.white,
-                            shape: BoxShape.circle,
-                            border: Border.all(
-                              color: CupertinoColors.white,
-                              width: 2,
-                            ),
-                          ),
-                          child: const CircleAvatar(
-                            radius: 48.0,
-                            backgroundImage: NetworkImage(
-                              "https://cdn.pixabay.com/photo/2023/06/13/15/05/astronaut-8061095_1280.png",
-                            ),
-                          ),
-                        ),
-                      ),
-                    ],
-                  ),
-                  const SizedBox(
-                    height: 10.0,
-                  ),
-                  const Text(
-                    "Emma",
-                    style: TextStyle(
-                      fontSize: 24.0,
-                      fontWeight: FontWeight.bold,
-                    ),
-                  ),
-                  const SizedBox(
-                    height: 8.0,
-                  ),
-                  const Text(
-                    "Emma@1234",
-                    style: TextStyle(
-                      color: CupertinoColors.systemGrey,
-                      fontSize: 16.0,
-                    ),
-                  ),
-                  const SizedBox(
-                    height: 16.0,
-                  ),
-                  const Text(
-                    "About",
-                    style: TextStyle(
-                      fontSize: 20.0,
-                      fontWeight: FontWeight.bold,
-                    ),
-                  ),
-                  const SizedBox(
-                    height: 8.0,
-                  ),
-                  const Text(
-                    "Hello. Everyone.",
-                    maxLines: 5,
-                    style: TextStyle(
-                      fontSize: 16.0,
-                    ),
-                  ),
-                  const SizedBox(
-                    height: 30.0,
-                  ),
-                ],
-              ),
-            ),
-          ],
-        ),
-      ),
+      body: userAccount != null ? buildUserProfile() : buildLoading(),
     );
   }
 
@@ -175,6 +79,106 @@ class _UserProfilePageState extends State<UserProfilePage> {
             CupertinoColors.activeGreen,
           ),
         ),
+      ),
+    );
+  }
+
+  Widget buildUserProfile() {
+    return SingleChildScrollView(
+      child: Stack(
+        children: [
+          Container(
+            height: 200.0,
+            decoration: const BoxDecoration(
+              image: DecorationImage(
+                image: NetworkImage(
+                  "https://cdn.pixabay.com/photo/2023/06/13/15/05/astronaut-8061095_1280.png",
+                ),
+                fit: BoxFit.cover,
+              ),
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.fromLTRB(16.0, 140.0, 16.0, 0),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    CircleAvatar(
+                      radius: 50.0,
+                      backgroundColor: CupertinoColors.white,
+                      backgroundImage: const NetworkImage(
+                        "https://cdn.pixabay.com/photo/2023/06/13/15/05/astronaut-8061095_1280.png",
+                      ),
+                      child: Container(
+                        padding: const EdgeInsets.all(2),
+                        decoration: BoxDecoration(
+                          color: CupertinoColors.white,
+                          shape: BoxShape.circle,
+                          border: Border.all(
+                            color: CupertinoColors.white,
+                            width: 2,
+                          ),
+                        ),
+                        child: const CircleAvatar(
+                          radius: 48.0,
+                          backgroundImage: NetworkImage(
+                            "https://cdn.pixabay.com/photo/2023/06/13/15/05/astronaut-8061095_1280.png",
+                          ),
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+                const SizedBox(
+                  height: 10.0,
+                ),
+                const Text(
+                  "Emma",
+                  style: TextStyle(
+                    fontSize: 24.0,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+                const SizedBox(
+                  height: 8.0,
+                ),
+                const Text(
+                  "Emma@1234",
+                  style: TextStyle(
+                    color: CupertinoColors.systemGrey,
+                    fontSize: 16.0,
+                  ),
+                ),
+                const SizedBox(
+                  height: 16.0,
+                ),
+                const Text(
+                  "About",
+                  style: TextStyle(
+                    fontSize: 20.0,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+                const SizedBox(
+                  height: 8.0,
+                ),
+                const Text(
+                  "Hello. Everyone.",
+                  maxLines: 5,
+                  style: TextStyle(
+                    fontSize: 16.0,
+                  ),
+                ),
+                const SizedBox(
+                  height: 30.0,
+                ),
+              ],
+            ),
+          ),
+        ],
       ),
     );
   }
