d2cb87c1fe91f363b6fb7696020019e234736c8b
Ituking <77528963+Ituking@users.noreply.github.com>
2023-07-03 01:55:09 +0900

    GridViewの実装（ユーザープロフィールページ）

--- lib/pages/user_profile_page.dart
+++ lib/pages/user_profile_page.dart
@@ -92,122 +92,143 @@ class _UserProfilePageState extends State<UserProfilePage> {
 
   Widget buildLoading() {
     return const Center(
-      child: Center(
-        child: CircularProgressIndicator(
-          strokeWidth: 4.0,
-          valueColor: AlwaysStoppedAnimation(
-            CupertinoColors.activeGreen,
-          ),
+      child: CircularProgressIndicator(
+        strokeWidth: 4.0,
+        valueColor: AlwaysStoppedAnimation(
+          CupertinoColors.activeGreen,
         ),
       ),
     );
   }
 
   Widget buildUserProfile() {
-    return SingleChildScrollView(
-      child: Stack(
-        children: [
-          Container(
-            height: 200.0,
-            decoration: const BoxDecoration(
-              image: DecorationImage(
-                image: NetworkImage(
-                  "https://cdn.pixabay.com/photo/2023/06/13/15/05/astronaut-8061095_1280.png",
+    return CustomScrollView(
+      slivers: [
+        SliverToBoxAdapter(
+          child: Stack(
+            children: [
+              Container(
+                height: 200.0,
+                decoration: const BoxDecoration(
+                  image: DecorationImage(
+                    image: NetworkImage(
+                      "https://cdn.pixabay.com/photo/2023/06/13/15/05/astronaut-8061095_1280.png",
+                    ),
+                    fit: BoxFit.cover,
+                  ),
                 ),
-                fit: BoxFit.cover,
               ),
-            ),
-          ),
-          Padding(
-            padding: const EdgeInsets.fromLTRB(16.0, 140.0, 16.0, 0),
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              Padding(
+                padding: const EdgeInsets.fromLTRB(16.0, 140.0, 16.0, 0),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
                   children: [
-                    CircleAvatar(
-                      radius: 50.0,
-                      backgroundColor: CupertinoColors.white,
-                      backgroundImage: const NetworkImage(
-                        "https://cdn.pixabay.com/photo/2023/06/13/15/05/astronaut-8061095_1280.png",
-                      ),
-                      child: Container(
-                        padding: const EdgeInsets.all(2),
-                        decoration: BoxDecoration(
-                          color: CupertinoColors.white,
-                          shape: BoxShape.circle,
-                          border: Border.all(
-                            color: CupertinoColors.white,
-                            width: 2,
-                          ),
-                        ),
-                        child: const CircleAvatar(
-                          radius: 48.0,
-                          backgroundImage: NetworkImage(
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        CircleAvatar(
+                          radius: 50.0,
+                          backgroundColor: CupertinoColors.white,
+                          backgroundImage: const NetworkImage(
                             "https://cdn.pixabay.com/photo/2023/06/13/15/05/astronaut-8061095_1280.png",
                           ),
+                          child: Container(
+                            padding: const EdgeInsets.all(2),
+                            decoration: BoxDecoration(
+                              color: CupertinoColors.white,
+                              shape: BoxShape.circle,
+                              border: Border.all(
+                                color: CupertinoColors.white,
+                                width: 2,
+                              ),
+                            ),
+                            child: const CircleAvatar(
+                              radius: 48.0,
+                              backgroundImage: NetworkImage(
+                                "https://cdn.pixabay.com/photo/2023/06/13/15/05/astronaut-8061095_1280.png",
+                              ),
+                            ),
+                          ),
                         ),
+                      ],
+                    ),
+                    const SizedBox(
+                      height: 10.0,
+                    ),
+                    const Text(
+                      "Emma",
+                      style: TextStyle(
+                        fontSize: 24.0,
+                        fontWeight: FontWeight.bold,
                       ),
+                    ),
+                    const SizedBox(
+                      height: 8.0,
+                    ),
+                    const Text(
+                      "Emma@1234",
+                      style: TextStyle(
+                        color: CupertinoColors.systemGrey,
+                        fontSize: 16.0,
+                      ),
+                    ),
+                    const SizedBox(
+                      height: 16.0,
+                    ),
+                    const Text(
+                      "About",
+                      style: TextStyle(
+                        fontSize: 20.0,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                    const SizedBox(
+                      height: 8.0,
+                    ),
+                    const Text(
+                      "Hello. Everyone.",
+                      maxLines: 5,
+                      style: TextStyle(
+                        fontSize: 16.0,
+                      ),
+                    ),
+                    const SizedBox(
+                      height: 30.0,
                     ),
                   ],
                 ),
-                const SizedBox(
-                  height: 10.0,
+              ),
+              const Padding(
+                padding: EdgeInsets.only(top: 400.0),
+                child: Divider(
+                  color: CupertinoColors.systemGrey,
                 ),
-                const Text(
-                  "Emma",
-                  style: TextStyle(
-                    fontSize: 24.0,
-                    fontWeight: FontWeight.bold,
-                  ),
-                ),
-                const SizedBox(
-                  height: 8.0,
-                ),
-                const Text(
-                  "Emma@1234",
-                  style: TextStyle(
-                    color: CupertinoColors.systemGrey,
-                    fontSize: 16.0,
-                  ),
-                ),
-                const SizedBox(
-                  height: 16.0,
-                ),
-                const Text(
-                  "About",
-                  style: TextStyle(
-                    fontSize: 20.0,
-                    fontWeight: FontWeight.bold,
-                  ),
-                ),
-                const SizedBox(
-                  height: 8.0,
-                ),
-                const Text(
-                  "Hello. Everyone.",
-                  maxLines: 5,
-                  style: TextStyle(
-                    fontSize: 16.0,
-                  ),
-                ),
-                const SizedBox(
-                  height: 30.0,
-                ),
-              ],
-            ),
+              ),
+            ],
           ),
-          const Padding(
-            padding: EdgeInsets.only(
-              top: 400.0,
+        ),
+        SliverPadding(
+          padding: EdgeInsets.zero,
+          sliver: SliverGrid.builder(
+            gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+              crossAxisCount: 3,
             ),
-            child: Divider(
-              color: CupertinoColors.activeGreen,
-            ),
+            itemBuilder: (context, index) {
+              Post post = postList[index];
+              return Container(
+                height: 200,
+                decoration: BoxDecoration(
+                  image: DecorationImage(
+                    image: NetworkImage(post.imagePath!),
+                    fit: BoxFit.cover,
+                  ),
+                ),
+              );
+            },
+            itemCount: postList.length,
           ),
-        ],
-      ),
+        ),
+      ],
     );
   }
 }
