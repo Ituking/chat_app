f955552fdb548488dd7e0496856be247c89f0543
Ituking <77528963+Ituking@users.noreply.github.com>
2023-07-02 23:50:05 +0900

    変数postListの定義

--- lib/pages/user_profile_page.dart
+++ lib/pages/user_profile_page.dart
@@ -1,6 +1,8 @@
 import 'package:chat_app/firestore/user_firestore.dart';
 import 'package:chat_app/model/account.dart';
+import 'package:chat_app/model/post.dart';
 import 'package:chat_app/model/user.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
@@ -15,6 +17,24 @@ class UserProfilePage extends StatefulWidget {
 
 class _UserProfilePageState extends State<UserProfilePage> {
   Account? userAccount;
+  List<Post> postList = [
+    Post(
+      id: "0001",
+      postAccountId: "0001",
+      postTime: Timestamp.now(),
+      imagePath:
+          "https://images.unsplash.com/photo-1471922694854-ff1b63b20054?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1472&q=80",
+      content: 'On a sunny day, you can still go to the beach 🌊.',
+    ),
+    Post(
+      id: "0002",
+      postAccountId: "0002",
+      postTime: Timestamp.now(),
+      imagePath:
+          "https://images.unsplash.com/photo-1542233637-20456b09d882?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=774&q=80",
+      content: 'A day healed by nature 🌳.',
+    ),
+  ];
 
   @override
   void initState() {
