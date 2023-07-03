63f58e240ee26ecfa51bfce6e0f5bef1d865c8fe
Ituking <77528963+Ituking@users.noreply.github.com>
2023-07-03 23:36:32 +0900

    変数postの定義

--- lib/pages/post_detail_page.dart
+++ lib/pages/post_detail_page.dart
@@ -1,8 +1,10 @@
+import 'package:chat_app/model/post.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 class PostDetailPage extends StatefulWidget {
-  const PostDetailPage({super.key});
+  final Post post;
+  const PostDetailPage({super.key, required this.post});
 
   @override
   State<PostDetailPage> createState() => _PostDetailPageState();
