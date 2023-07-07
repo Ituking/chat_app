92d75744b1a42286bf5c3e6c14c71fcf9dce8727
Ituking <77528963+Ituking@users.noreply.github.com>
2023-07-07 11:11:28 +0900

    縦スクロール可能に実装（投稿詳細ページ）

--- lib/pages/post_detail_page.dart
+++ lib/pages/post_detail_page.dart
@@ -27,74 +27,77 @@ class _PostDetailPageState extends State<PostDetailPage> {
           ),
         ),
       ),
-      body: Column(
-        children: [
-          const ListTile(
-            leading: CircleAvatar(
-              backgroundImage: NetworkImage(
-                "https://cdn.pixabay.com/photo/2023/06/13/15/05/astronaut-8061095_1280.png",
+      body: SingleChildScrollView(
+        scrollDirection: Axis.vertical,
+        child: Column(
+          children: [
+            const ListTile(
+              leading: CircleAvatar(
+                backgroundImage: NetworkImage(
+                  "https://cdn.pixabay.com/photo/2023/06/13/15/05/astronaut-8061095_1280.png",
+                ),
+              ),
+              title: Text(
+                "Emma",
+                style: TextStyle(
+                  fontWeight: FontWeight.bold,
+                  color: CupertinoColors.black,
+                ),
               ),
             ),
-            title: Text(
-              "Emma",
-              style: TextStyle(
-                fontWeight: FontWeight.bold,
-                color: CupertinoColors.black,
-              ),
-            ),
-          ),
-          GestureDetector(
-            onTap: () {
-              Navigator.push(
-                context,
-                MaterialPageRoute(
-                  builder: (context) => ImageZoomPage(
-                    imagePath: widget.post.imagePath,
+            GestureDetector(
+              onTap: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                    builder: (context) => ImageZoomPage(
+                      imagePath: widget.post.imagePath,
+                    ),
                   ),
-                ),
-              );
-            },
-            child: SizedBox(
-              height: MediaQuery.of(context).size.height * 0.5,
-              child: Padding(
-                padding: const EdgeInsets.symmetric(
-                  horizontal: 10.0,
-                  vertical: 10.0,
-                ),
-                child: Hero(
-                  tag: "post_${widget.post.id}",
-                  child: Container(
-                    height: 200,
-                    decoration: BoxDecoration(
-                      borderRadius: BorderRadius.circular(24.0),
-                      image: DecorationImage(
-                        image: NetworkImage(widget.post.imagePath!),
-                        fit: BoxFit.cover,
+                );
+              },
+              child: SizedBox(
+                height: MediaQuery.of(context).size.height * 0.5,
+                child: Padding(
+                  padding: const EdgeInsets.symmetric(
+                    horizontal: 10.0,
+                    vertical: 10.0,
+                  ),
+                  child: Hero(
+                    tag: "post_${widget.post.id}",
+                    child: Container(
+                      height: 200,
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(24.0),
+                        image: DecorationImage(
+                          image: NetworkImage(widget.post.imagePath!),
+                          fit: BoxFit.cover,
+                        ),
                       ),
                     ),
                   ),
                 ),
               ),
             ),
-          ),
-          Padding(
-            padding: const EdgeInsets.symmetric(
-              horizontal: 8.0,
-              vertical: 25.0,
-            ),
-            child: Container(
-              alignment: Alignment.centerLeft,
-              width: double.infinity,
-              child: Text(
-                widget.post.content,
-                style: const TextStyle(
-                  fontSize: 16.0,
-                  color: CupertinoColors.black,
+            Padding(
+              padding: const EdgeInsets.symmetric(
+                horizontal: 8.0,
+                vertical: 25.0,
+              ),
+              child: Container(
+                alignment: Alignment.centerLeft,
+                width: double.infinity,
+                child: Text(
+                  widget.post.content,
+                  style: const TextStyle(
+                    fontSize: 16.0,
+                    color: CupertinoColors.black,
+                  ),
                 ),
               ),
             ),
-          ),
-        ],
+          ],
+        ),
       ),
     );
   }
