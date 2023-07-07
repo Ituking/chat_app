6d46edc84b019d07765df8d0677d74bd8805a6ae
Ituking <77528963+Ituking@users.noreply.github.com>
2023-07-07 09:24:20 +0900

    ユーザーアイコンの実装（投稿詳細ページ）

--- lib/pages/post_detail_page.dart
+++ lib/pages/post_detail_page.dart
@@ -27,39 +27,50 @@ class _PostDetailPageState extends State<PostDetailPage> {
           ),
         ),
       ),
-      body: GestureDetector(
-        onTap: () {
-          Navigator.push(
-            context,
-            MaterialPageRoute(
-              builder: (context) => ImageZoomPage(
-                imagePath: widget.post.imagePath,
+      body: Column(
+        children: [
+          const ListTile(
+            leading: CircleAvatar(
+              backgroundImage: NetworkImage(
+                "https://cdn.pixabay.com/photo/2023/06/13/15/05/astronaut-8061095_1280.png",
               ),
             ),
-          );
-        },
-        child: SizedBox(
-          height: MediaQuery.of(context).size.height * 0.5,
-          child: Padding(
-            padding: const EdgeInsets.symmetric(
-              horizontal: 10.0,
-              vertical: 10.0,
-            ),
-            child: Hero(
-              tag: "post_${widget.post.id}",
-              child: Container(
-                height: 200,
-                decoration: BoxDecoration(
-                  borderRadius: BorderRadius.circular(24.0),
-                  image: DecorationImage(
-                    image: NetworkImage(widget.post.imagePath!),
-                    fit: BoxFit.cover,
+          ),
+          GestureDetector(
+            onTap: () {
+              Navigator.push(
+                context,
+                MaterialPageRoute(
+                  builder: (context) => ImageZoomPage(
+                    imagePath: widget.post.imagePath,
+                  ),
+                ),
+              );
+            },
+            child: SizedBox(
+              height: MediaQuery.of(context).size.height * 0.5,
+              child: Padding(
+                padding: const EdgeInsets.symmetric(
+                  horizontal: 10.0,
+                  vertical: 10.0,
+                ),
+                child: Hero(
+                  tag: "post_${widget.post.id}",
+                  child: Container(
+                    height: 200,
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(24.0),
+                      image: DecorationImage(
+                        image: NetworkImage(widget.post.imagePath!),
+                        fit: BoxFit.cover,
+                      ),
+                    ),
                   ),
                 ),
               ),
             ),
           ),
-        ),
+        ],
       ),
     );
   }
