1efd1586f0e56bfc4d72505bafa338997be8bdf9
Ituking <77528963+Ituking@users.noreply.github.com>
2023-07-09 23:23:11 +0900

    全体的な余白の修正（トップページ）

--- lib/pages/top_page.dart
+++ lib/pages/top_page.dart
@@ -19,104 +19,112 @@ class _TopPageState extends State<TopPage> {
     return Scaffold(
       appBar: WidgetUtils.createAppBar("Chats"),
       body: StreamBuilder<QuerySnapshot>(
-          stream: RoomFireStore.joinedRoomSnapshot,
-          builder: (context, streamSnapshot) {
-            if (streamSnapshot.hasData) {
-              return FutureBuilder<List<TalkRoom>?>(
-                  future: RoomFireStore.fetchJoinedRooms(streamSnapshot.data!),
-                  builder: (context, futureSnapshot) {
-                    if (futureSnapshot.connectionState ==
-                        ConnectionState.waiting) {
-                      return const Center(
-                        child: CircularProgressIndicator(),
-                      );
-                    } else {
-                      if (futureSnapshot.hasData) {
-                        List<TalkRoom> talkRooms = futureSnapshot.data!;
-                        return SizedBox(
-                          height: MediaQuery.of(context).size.height,
-                          child: ListView.builder(
-                              itemCount: talkRooms.length,
-                              itemBuilder: (context, index) {
-                                if (kDebugMode) {
-                                  print(
-                                    "talkRooms[index].talkUser.imagePath => ${talkRooms[index].talkUser.imagePath}",
+        stream: RoomFireStore.joinedRoomSnapshot,
+        builder: (context, streamSnapshot) {
+          if (streamSnapshot.hasData) {
+            return FutureBuilder<List<TalkRoom>?>(
+                future: RoomFireStore.fetchJoinedRooms(streamSnapshot.data!),
+                builder: (context, futureSnapshot) {
+                  if (futureSnapshot.connectionState ==
+                      ConnectionState.waiting) {
+                    return const Center(
+                      child: CircularProgressIndicator(),
+                    );
+                  } else {
+                    if (futureSnapshot.hasData) {
+                      List<TalkRoom> talkRooms = futureSnapshot.data!;
+                      return SizedBox(
+                        height: MediaQuery.of(context).size.height,
+                        child: ListView.builder(
+                            itemCount: talkRooms.length,
+                            itemBuilder: (context, index) {
+                              if (kDebugMode) {
+                                print(
+                                  "talkRooms[index].talkUser.imagePath => ${talkRooms[index].talkUser.imagePath}",
+                                );
+                              }
+                              return InkWell(
+                                onTap: () {
+                                  Navigator.push(
+                                    context,
+                                    MaterialPageRoute(
+                                      builder: (context) => TalkRoomPage(
+                                        talkRoom: talkRooms[index],
+                                      ),
+                                    ),
                                   );
-                                }
-                                return InkWell(
-                                  onTap: () {
-                                    Navigator.push(
-                                      context,
-                                      MaterialPageRoute(
-                                        builder: (context) => TalkRoomPage(
-                                          talkRoom: talkRooms[index],
+                                },
+                                child: SizedBox(
+                                  height: 70,
+                                  child: Row(
+                                    children: [
+                                      Padding(
+                                        padding: const EdgeInsets.fromLTRB(
+                                          16.0,
+                                          0,
+                                          8.0,
+                                          0,
+                                        ),
+                                        child: CircleAvatar(
+                                          radius: 25,
+                                          backgroundImage: talkRooms[index]
+                                                      .talkUser
+                                                      .imagePath !=
+                                                  null
+                                              ? NetworkImage(talkRooms[index]
+                                                  .talkUser
+                                                  .imagePath!)
+                                              : null,
                                         ),
                                       ),
-                                    );
-                                  },
-                                  child: SizedBox(
-                                    height: 70,
-                                    child: Row(
-                                      children: [
-                                        Padding(
-                                          padding: const EdgeInsets.symmetric(
-                                              horizontal: 8.0),
-                                          child: CircleAvatar(
-                                            radius: 25,
-                                            backgroundImage: talkRooms[index]
-                                                        .talkUser
-                                                        .imagePath !=
-                                                    null
-                                                ? NetworkImage(talkRooms[index]
-                                                    .talkUser
-                                                    .imagePath!)
-                                                : null,
-                                          ),
-                                        ),
-                                        Expanded(
-                                          child: Column(
-                                            crossAxisAlignment:
-                                                CrossAxisAlignment.start,
-                                            mainAxisAlignment:
-                                                MainAxisAlignment.center,
-                                            children: [
-                                              Text(
-                                                talkRooms[index].talkUser.name,
-                                                style: const TextStyle(
-                                                  fontWeight: FontWeight.bold,
-                                                ),
+                                      const SizedBox(
+                                        width: 5,
+                                      ),
+                                      Expanded(
+                                        child: Column(
+                                          crossAxisAlignment:
+                                              CrossAxisAlignment.start,
+                                          mainAxisAlignment:
+                                              MainAxisAlignment.center,
+                                          children: [
+                                            Text(
+                                              talkRooms[index].talkUser.name,
+                                              style: const TextStyle(
+                                                fontWeight: FontWeight.bold,
                                               ),
-                                              Text(
-                                                talkRooms[index].lastMessage ??
-                                                    "",
-                                                style: const TextStyle(
-                                                  color: Colors.grey,
-                                                ),
-                                                maxLines: 1,
-                                                overflow: TextOverflow.ellipsis,
+                                            ),
+                                            Text(
+                                              talkRooms[index].lastMessage ??
+                                                  "",
+                                              style: const TextStyle(
+                                                color: Colors.grey,
                                               ),
-                                            ],
-                                          ),
+                                              maxLines: 1,
+                                              overflow: TextOverflow.ellipsis,
+                                            ),
+                                          ],
                                         ),
-                                      ],
-                                    ),
+                                      ),
+                                    ],
                                   ),
-                                );
-                              }),
-                        );
-                      } else {
-                        return const Center(
-                          child: Text("FAILED"),
-                        );
-                      }
+                                ),
+                              );
+                            }),
+                      );
+                    } else {
+                      return const Center(
+                        child: Text("FAILED"),
+                      );
                     }
-                  });
-            } else {
-              return const Center(
-                child: CircularProgressIndicator(),
-              );
-            }
-          }),
+                  }
+                });
+          } else {
+            return const Center(
+              child: CircularProgressIndicator(),
+            );
+          }
+        },
+      ),
     );
   }
 }
