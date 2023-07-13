711d496c28616d7424de8e34c56881a9231e0cac
Ituking <77528963+Ituking@users.noreply.github.com>
2023-07-14 00:00:02 +0900

    自己紹介文を動的に実装（ユーザープロフィールページ）

--- lib/pages/user_profile_page.dart
+++ lib/pages/user_profile_page.dart
@@ -241,10 +241,10 @@ class _UserProfilePageState extends State<UserProfilePage> {
                     const SizedBox(
                       height: 8.0,
                     ),
-                    const Text(
-                      "Hello. Everyone.",
+                    Text(
+                      userAccount!.selfIntroduction,
                       maxLines: 5,
-                      style: TextStyle(
+                      style: const TextStyle(
                         fontSize: 16.0,
                       ),
                     ),
