219e73d92d1ff1a8c0a8fdbe01c9186c66e3dfcd
Ituking <77528963+Ituking@users.noreply.github.com>
2023-08-19 10:42:38 +0900

    いらないものの削除

--- lib/utils/like_button.dart
+++ lib/utils/like_button.dart
@@ -1,7 +1,5 @@
 import 'package:chat_app/model/post.dart';
-import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/cupertino.dart';
-import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 
 class LikeButton extends StatefulWidget {
@@ -13,63 +11,6 @@ class LikeButton extends StatefulWidget {
 }
 
 class _LikeButtonState extends State<LikeButton> {
-  late bool isLiked;
-
-  @override
-  void initState() {
-    super.initState();
-    isLiked = widget.post.likedUserIds.contains(widget.post.postAccountId);
-    if (kDebugMode) {
-      print("isLiked: $isLiked");
-      print("widget.post.postAccountId: ${widget.post.postAccountId}");
-      print("widget.post.likedUserIds: ${widget.post.likedUserIds}");
-    }
-  }
-
-  void toggleLike() async {
-    // setState(() {
-    //   List<String> updatedLikedUserIds = List.from(widget.post.likedUserIds);
-    //   if (kDebugMode) {
-    //     print("postAccountId: ${widget.post.postAccountId}");
-    //     print("likedUserIds: ${widget.post.likedUserIds}");
-    //   }
-    //   if (isLiked) {
-    //     widget.post.likedUserIds.remove(widget.post.postAccountId);
-    //     widget.post.likedCount--;
-    //   } else {
-    //     widget.post.likedUserIds.add(widget.post.postAccountId);
-    //     widget.post.likedCount++;
-    //   }
-    //   widget.post.likedCount = widget.post.likedUserIds.length;
-    //   if (kDebugMode) {
-    //     print("updatedLikedUserIds: $updatedLikedUserIds");
-    //     print("widget.post.likedUserIds: ${widget.post.likedUserIds}");
-    //   }
-    //   isLiked = !isLiked;
-    // });
-
-    try {
-      final ref =
-          FirebaseFirestore.instance.collection('posts').doc(widget.post.id);
-
-      final updateData = {
-        'liked_count': FieldValue.increment(isLiked ? -1 : 1),
-        'liked_user_ids': isLiked
-            ? FieldValue.arrayRemove([widget.post.likedUserIds])
-            : FieldValue.arrayUnion([widget.post.likedUserIds]),
-      };
-
-      await ref.update(updateData);
-      if (kDebugMode) {
-        print("Firestore data updated successfully");
-      }
-    } on FirebaseException catch (e) {
-      if (kDebugMode) {
-        print("Firestore update error: $e");
-      }
-    }
-  }
-
   @override
   Widget build(BuildContext context) {
     return IconButton(
