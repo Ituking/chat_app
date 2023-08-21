31e13dacd52b56e22405b85e694746546089a9d9
Ituking <77528963+Ituking@users.noreply.github.com>
2023-08-21 14:50:58 +0900

    StatelessWidgetへの置き換えに伴うconstの削除

--- lib/pages/image_zoom_page.dart
+++ lib/pages/image_zoom_page.dart
@@ -33,7 +33,7 @@ class _ImageZoomPageState extends State<ImageZoomPage> {
                 Navigator.pop(
                   context,
                   MaterialPageRoute(
-                    builder: (context) => const HomePage(),
+                    builder: (context) => HomePage(),
                   ),
                 );
               },
