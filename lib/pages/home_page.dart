a2f1d44b4d331334658d03cd172505459e207540
Ituking <77528963+Ituking@users.noreply.github.com>
2023-08-27 16:21:45 +0900

    initState内でgetPostUserMapが処理されるように実装

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -30,6 +30,7 @@ class _HomePageState extends State<HomePage> {
   @override
   void initState() {
     super.initState();
+    getPostUserMap = AccountFirestore.getPostUserMap(postAccountIds);
   }
 
   @override
